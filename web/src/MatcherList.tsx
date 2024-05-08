7aed7cbed43f9f4501cd3fafdb9afe757b0d632f
Tai <letientai299@gmail.com>
2024-05-09 06:58:19 +0700

    Config github page

--- web/src/MatcherList.tsx
+++ web/src/MatcherList.tsx
@@ -87,21 +87,32 @@ function Guide() {
   return (
     <ul>
       <li>
-        <strong>Query</strong>: can be normal string or a valid JS regular
-        expression, e.g. <code>/p\w+g/ig</code>. Internally, the code construct
-        the <code>RegExp</code> object by simple remove first <code>/</code>,
-        then split the expression and flags at the last <code>/</code>.
+        <p>
+          <strong>Query</strong>: can be normal string or a valid JS regular
+          expression, e.g. <code>/p\w+g/ig</code>. Internally, the code
+          construct the <code>RegExp</code> object by simple remove first{' '}
+          <code>/</code>, then split the expression and flags at the last{' '}
+          <code>/</code>.
+        </p>
       </li>
       <li>
-        <strong>Options</strong>: Most option are simply an example usage of the
-        provided <code>match</code> function, which will scan the text using the
-        query to look for parts to apply different decorations. The exception is{' '}
-        <code>typo</code>. It uses{' '}
-        <a href="https://github.com/cfinke/Typo.js">Typo.js</a> to check for
-        misspelled English words and add a tooltip to show suggestions. It's
-        quite slow, will freeze the UI on the first time. If the query is not
-        empty, <code>typo</code> will only spell-check those words match the
-        query.
+        <p>
+          <strong>Options</strong>: Most option are simply an example usage of
+          the provided <code>match</code> function, which will scan the text
+          using the query to look for parts to apply different decorations.
+        </p>
+        <p>
+          The exception is <code>typo</code>. It uses{' '}
+          <a href="https://github.com/cfinke/Typo.js">Typo.js</a> to check for
+          misspelled English words and add a tooltip to show suggestions. It's
+          quite slow, will freeze the UI and the suggestions might be even
+          wrong. Its purpose is to show an advanced use case of the library.
+        </p>
+
+        <p>
+          If the query is not empty, <code>typo</code> will only spell-check
+          those words match the query.
+        </p>
       </li>
     </ul>
   );
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,7 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/react-decorated-text' : '',
   plugins: [react()],
   resolve: {
     alias: {
